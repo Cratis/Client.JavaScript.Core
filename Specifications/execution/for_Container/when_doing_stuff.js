0cf5728e3d19cd4aa976f5f31b0b7e29023a70a3
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-02 18:42:51 +0100

    Cleaning up

--- Specifications/execution/for_Container/when_doing_stuff.js
+++ Specifications/execution/for_Container/when_doing_stuff.js
@@ -4,6 +4,8 @@ describe("when doing stuff", () => {
    var container = new Container();
    
    container.sayHello();
+   
+   console.log("FGromfasd");
       
    it("should do stuff", () => {
        expect(true).toBe(true);
