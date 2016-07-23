5593ba9b31b28e01fa8e33cf7d71061f8ad311a5
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-23 16:29:58 -0700

    Wallaby test running up and running

--- wallaby.js
+++ wallaby.js
@@ -1,11 +1,18 @@
+// http://ilikekillnerds.com/2016/04/webpack-typescript-aurelia-wallaby-js/
+// https://github.com/wallabyjs/wallaby-jspm-sample/blob/master/wallaby.js
 module.exports = function (wallaby) {
     return {
         files: [
             { pattern: "jspm_packages/system.js", instrument: false },
+            { pattern: "node_modules/chai/chai.js", instrument: false },
+            { pattern: "node_modules/chai-as-promised/chai-as-promised.js", instrument: false },
             { pattern: "config.js", instrument: false },
+
+            { pattern: "Specifications/**/given/*.js", load: false },
             { pattern: "Source/**/*.js", load: false }
         ],
         tests: [
+            { pattern: "!Specifications/**/given/*.js", load: false },
             { pattern: "Specifications/**/*.js", load: false }
         ],
 
@@ -15,6 +22,10 @@ module.exports = function (wallaby) {
             })
         },
 
+        env: {
+            //type: "node"
+            kind: "electron"
+        },
 
         middleware: (app, express) => {
             app.use("/jspm_packages", express.static(require("path").join(__dirname, "jspm_packages")));
@@ -23,23 +34,32 @@ module.exports = function (wallaby) {
         setup: (wallaby) => {
             wallaby.delayStart();
 
+            window.expect = chai.expect;
+            var should = chai.should();
+
             System.config({
                 transpiler: "none"
             });
 
-            var promises = [];
-            for (var i = 0, len = wallaby.tests.length; i < len; i++) {
-                promises.push(System['import'](wallaby.tests[i]));
-            }
+            System.import("sinon").then(function (s) {
+                window.sinon = s;
 
-            Promise.all(promises).then(function () {
-                wallaby.start();
-            }).catch(function (e) {
-                setTimeout(function () {
-                    throw e;
-                }, 0);
+                var promises = [];
+                for (var i = 0, len = wallaby.tests.length; i < len; i++) {
+                    promises.push(System['import'](wallaby.tests[i]));
+                }
+
+                Promise.all(promises).then(function () {
+                    wallaby.start();
+                }).catch(function (e) {
+                    setTimeout(function () {
+                        throw e;
+                    }, 0);
+                });
             });
         },
+
+        testFramework: "jasmine",
 
         debug: true
     };
